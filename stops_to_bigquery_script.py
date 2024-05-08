74c82756d4de3f4111045477d21b002858be6b23
Sharvil21 <46630953+Sharvil21@users.noreply.github.com>
2024-05-08 18:38:38 -0400

    Update stops_to_bigquery_script.py

--- stops_to_bigquery_script.py
+++ stops_to_bigquery_script.py
@@ -1,5 +1,5 @@
 #This code fetches the data from bus stops location API. It is to be written in Cloud Functions.
-
+# We get the Stops data from the API in the form of a zip file. Then another Python function extracts that zip file. And then this code takes the stops.txt file and loads data into BigQuery
 from google.cloud import bigquery
 import json
 
