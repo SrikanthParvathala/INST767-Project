7833115e4a1b9ad7791b2301bbbcc06a9b3e163b
Sharvil21 <46630953+Sharvil21@users.noreply.github.com>
2024-05-13 11:54:31 -0400

    Delete folder1 directory

--- folder1/stops_to_bigquery_script.py
+++
@@ -1,2 +0,0 @@
-#This code fetches the data from bus stops location API. It is to be written in Cloud Functions.
-# We get the Stops data from the API in the form of a zip file. Then another Python function extracts that zip file. And then this code takes the stops.txt file and loads data into BigQuery
